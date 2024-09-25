d5690bcea44c90365141e99906e4b94c720f10aa
TheMiranha <lxbmiranda@gmail.com>
2024-09-25 16:56:24 -0300

    add path

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { useRef } from "react";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { useCallback, useEffect, useState } from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { Icon } from "leaflet";
-
-const legalIcon = new Icon({
-  iconUrl: 'Unicorn-3d-icon.png',
-  iconSize: [35, 35],
-  popupAnchor: [-3, -76],
-})
+import { Map, polyline } from "leaflet";
+import { antPath } from 'leaflet-ant-path';
 
 const points: { latitude: number, longitude: number, text: string }[] = [
   { latitude: -22.68882484078866, longitude: -47.6328992843628, text: 'Ponto 1' },
@@ -20,21 +15,39 @@ const points: { latitude: number, longitude: number, text: string }[] = [
   { latitude: -22.6820, longitude: -47.6338, text: 'Ponto 1' },
   { latitude: -22.6810, longitude: -47.6342, text: 'Ponto 1' },
   { latitude: -22.6800, longitude: -47.6338, text: 'Ponto 1' },
-  { latitude: -22.685, longitude: -47.605, text: 'Ponto 2' },
 ]
 
+const path = antPath([points.map(point => [point.latitude, point.longitude])], {
+  use: polyline,
+})
+
 const ExemploDeMapa = () => {
-  const mapRef = useRef(null);
+
+  const [mapInstance, setMapInstance] = useState<Map | null>(null)
+  const [loadedPath, setLoadedPath] = useState<boolean>(false)
+
+  useEffect(() => {
+    if (mapInstance && !loadedPath) {
+      mapInstance.addLayer(path)
+      setLoadedPath(true)
+    }
+  }, [mapInstance, loadedPath])
+
+  const handleRect = useCallback((node: Map) => {
+    if (node) {
+      setMapInstance(node)
+    }
+  }, []);
 
   return (
     <div style={{ height: '100dvh', width: '100dvw', display: 'grid', placeItems: 'center', backgroundColor: '#242424' }}>
-      <MapContainer center={[points[0].latitude, points[0].longitude]} zoom={13} ref={mapRef} style={{ height: "75vh", width: "75vw" }}>
+      <MapContainer center={[points[0].latitude, points[0].longitude]} zoom={13} ref={handleRect} style={{ height: "75vh", width: "75vw" }}>
         <TileLayer
           attribution='Qualquer texto'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
-        {
+        {/* {
           points.map(point => (
             <Marker icon={legalIcon} position={[point.latitude, point.longitude]} key={point.latitude + '-' + point.longitude}>
               <Popup>
@@ -42,7 +55,7 @@ const ExemploDeMapa = () => {
               </Popup>
             </Marker>
           ))
-        }
+        } */}
 
       </MapContainer>
     </div>
